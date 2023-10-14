086367b8a84f9f6c0d3833c0511f5f378fdbd328
UNI-shibuya-taku <s.takuuuuu@gmail.com>
2023-10-14 13:21:34 +0900

    edit

--- include/nav_manager/global_path_creator.h
+++ include/nav_manager/global_path_creator.h
@@ -8,10 +8,10 @@
 #include "nav_msgs/Path.h"
 #include "geometry_msgs/PointStamped.h"
 #include <std_msgs/Int32MultiArray.h>
-#include <amsl_navigation_msgs/Node.h>
-#include <amsl_navigation_msgs/Edge.h>
-#include <amsl_navigation_msgs/NodeEdgeMap.h>
-#include <amsl_navigation_msgs/Replan.h>
+// #include <amsl_navigation_msgs/Node.h>
+// #include <amsl_navigation_msgs/Edge.h>
+// #include <amsl_navigation_msgs/NodeEdgeMap.h>
+// #include <amsl_navigation_msgs/Replan.h>
 class GlobalPathCreator
 {
 public:
@@ -22,27 +22,32 @@ private:
     // void map_callback(const nav_msgs::OccupancyGrid::ConstPtr&);
     // void path_callback(const std_msgs::Int32MultiArray::ConstPtr&);
     // void node_edge_map_callback(const amsl_navigation_msgs::NodeEdgeMapConstPtr&);
-    // void make_global_path();
+    void load_waypoints();
+    void load_route();
 
     bool replan_flag;
     bool node_edge_flag;
     bool global_path_flag;
-    ros::NodeHandle n;
-    ros::NodeHandle private_n;
+    std::vector<int> routes_;
+    std::vector<Waypoint> waypoints_;
+
+    XmlRpc::XmlRpcValue waypoints_list_;
+    XmlRpc::XmlRpcValue route_list_;
+
+    // ros::NodeHandle n;
+    ros::NodeHandle private_nh_;
   
     ros::Publisher pub_path;
-    ros::Publisher pub_open_grid;
-    ros::Subscriber sub_map;
-    ros::Subscriber sub_path;
-    ros::Subscriber node_edge_map_sub;
-    ros::Subscriber check_point_sub;
+    // ros::Publisher pub_open_grid;
+    // ros::Subscriber sub_map;
+    // ros::Subscriber sub_path;
+    // ros::Subscriber node_edge_map_sub;
+    // ros::Subscriber check_point_sub;
 
     nav_msgs::Path global_path;
     nav_msgs::Path waypoint_path;
-    amsl_navigation_msgs::NodeEdgeMap map;
-    std_msgs::Int32MultiArray check_points;
-    std_msgs::Int32MultiArray global_path_num;
+    // amsl_navigation_msgs::NodeEdgeMap map;
+    // std_msgs::Int32MultiArray check_points;
+    // std_msgs::Int32MultiArray global_path_num;
 };
-
-
 #endif// __SIMPLE_LOCALMAP_CREATOR_H
--- src/global_path_creator.cpp
+++ src/global_path_creator.cpp
@@ -1,19 +1,59 @@
 #include "nav_manager/global_path_creator.h"
+#include "nav_manager/waypoints.h"
+
 GlobalPathCreator::GlobalPathCreator() :
 	private_nh_("~"),
-	has_received_odom_(false), has_received_imu_(false), has_received_ndt_pose_(false),
-	is_first_(true), is_respawn_(false)
+	// has_received_odom_(false), has_received_imu_(false), has_received_ndt_pose_(false),
+	// is_first_(true), is_respawn_(false)
 {
-	private_nh_.param("respawn_pose_topic_name",respawn_pose_topic_name_,{"/position/respawn"});
-	private_nh_.param("MOTION_NOISE_OO",MOTION_NOISE_OO_,{1e-3});
+	// private_nh_.param("respawn_pose_topic_name",respawn_pose_topic_name_,{"/position/respawn"});
+	// private_nh_.param("MOTION_NOISE_OO",MOTION_NOISE_OO_,{1e-3});
 
 	// ndt_pose_sub_ = nh_.subscribe(ndt_pose_topic_name_,10,&EKF::ndt_pose_callback,this);
     pub_path = n.advertise<nav_msgs::Path>("/global_path/path_path",1);
-
-	// broadcaster_.reset(new tf2_ros::TransformBroadcaster);
-	// buffer_.reset(new tf2_ros::Buffer);
-	// listener_.reset(new tf2_ros::TransformListener(*buffer_));
-
+    load_waypoints();
+    load_route();
+}
+void GlobalPathCreator::load_waypoints()
+{
+    if(!private_nh_.getParam("waypoints_list",waypoints_list_)){
+        ROS_WARN("Cloud not load waypoints list");
+        return;
+    }
+    ROS_ASSERT(waypoints_list_.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    for(int i = 0; i < (int)waypoints_list_.size(); i++){
+        if(!waypoints_list_[i]["id"].valid() || !waypoints_list_[i]["x"].valid() || !waypoints_list_[i]["y"].valid()){
+            ROS_WARN("waypoints list is valid");
+            return;
+        }
+        if(waypoints_list_[i]["id"].getType() == XmlRpc::XmlRpcValue::TypeInt && waypoints_list_[i]["x"].getType() == XmlRpc::XmlRpcValue::TypeInt && waypoints_list_[i]["y"].getType() == XmlRpc::XmlRpcValue::TypeInt){
+            int id = static_cast<int>(waypoints_list_[i]["id"]);
+            double x = static_cast<double>(waypoints_list_[i]["x"]);
+            double y = static_cast<double>(waypoints_list_[i]["y"]);
+            std::cout << "id: " << id << " x: " << x << " y: " << y << std::endl;
+            Waypoint waypoint(id, x, y);
+            waypoints_.push_back(waypoint);
+        }
+    }
+}
+void GlobalPathCreator::load_route()
+{
+    if(!private_nh_.getParam("route_list",route_list_)){
+        ROS_WARN("Cloud not load route list");
+        return;
+    }
+    ROS_ASSERT(route_list_.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    for(int i = 0; i < (int)route_list_.size(); i++){
+        if(!route_list_[i]["id"].valid()){
+            ROS_WARN("route list is valid");
+            return;
+        }
+        if(route_list_[i]["id"].getType() == XmlRpc::XmlRpcValue::TypeInt){
+            int id = static_cast<int>(route_list_[i]["id"]);
+            std::cout << "route_liset_: " << id << std::endl;
+            routes_.push_back(id);
+        }
+    }
 }
 
 void GlobalPathCreator::process()
